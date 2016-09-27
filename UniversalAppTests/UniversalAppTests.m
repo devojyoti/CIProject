7e96d2fae28658a6c396b1e8f5133b43844b9aa1
Nazmul Quader Zinnuree <zinnuree@gmail.com>
2016-09-27 19:54:37 +0600

    test added

--- UniversalAppTests/UniversalAppTests.m
+++ UniversalAppTests/UniversalAppTests.m
@@ -25,8 +25,11 @@
 }
 
 - (void)testExample {
+    
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    XCTAssert(3 == 5, @"Failed");
 }
 
 - (void)testPerformanceExample {
